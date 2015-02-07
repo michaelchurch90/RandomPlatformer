becf846ca29b3c39de68ca2292d0d672780d50c1
Michael Church <michaelchurch90@gmail.com>
2015-02-06 19:15:23 -0500

    Added all assets I have created

---
+++ Assets/Scripts/DoubleJumpShoes.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoubleJumpShoes : MonoBehaviour {
+
+
+	Transform leftFoot;
+	Transform playerLeftFoot;
+	Transform rightFoot;
+	Transform player;
+	void Start()
+	{
+		leftFoot = GetComponentInChildren<Transform> ().Find ("PlayerAtlas_ShoeLeft");
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+	
+		player = other.GetComponentInParent<Transform>();
+		//transform.localScale = new Vector3 (1, 1, 1);
+
+		playerLeftFoot = player.Find ("PlayerAtlas_LegLeft/PlayerAtlas_FootLeft");
+
+		playerLeftFoot = leftFoot;
+		leftFoot.parent = playerLeftFoot;
+
+
+		//transform.localScale.y = 1;
+	}
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour {
+	BoxCollider2D trigger;
+	Transform spawnLocation;
+	Vector3 bounds;
+	void Start()
+	{
+		trigger = GetComponent<BoxCollider2D> ();
+		spawnLocation = GetComponent<Transform> ();
+
+
+
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+	
+
+		trigger.enabled = false;
+		Debug.Log ("Loading new Scene");
+		int roomID = Random.Range (0, 2);
+		UnityEngine.Object pPrefab = null;;
+
+		if (roomID == 0) 
+		{
+			pPrefab = Resources.Load ("Rooms/FlatRoom");
+		}
+		if (roomID == 1) 
+		{
+			pPrefab = Resources.Load ("Rooms/UpStairsRoom");
+			//pPrefab = Resources.Load ("Rooms/FlatRoom");
+		}
+	
+
+		GameObject nextScreen = (GameObject)GameObject.Instantiate (pPrefab, spawnLocation.position, Quaternion.identity);
+		
+	}
+
+
+}
